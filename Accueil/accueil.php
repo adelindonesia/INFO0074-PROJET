729ee58a85a893d1f14ea8435160e4f58ac952ef
adelindonesia <adelia_rahmawati12@outlook.com>
2021-04-11 10:25:42 +0200

    Modif css et nav bar

---
+++ Accueil/accueil.php
@@ -0,0 +1,33 @@
+<?php
+	include 'header.php';
+?>
+<main>
+	<!--Sous banner-->
+	<div class="wrapper">
+		<section class="index-links">
+			<a href="./produits.php">
+				<div class="index-boxlink-square">
+					<h3>Produits</h3>
+				</div>
+			</a>
+			<a href="./panier.php">
+				<div class="index-boxlink-rectangle">
+					<h3>Panier</h3>
+				</div>
+			</a>
+			<a href="./login.php">
+				<div class="index-boxlink-rectangle">
+					<h3>Mon Compte</h3>
+				</div>
+			</a>
+			<a href="./Contactez-nous.php">
+				<div class="index-boxlink-square">
+					<h3>Contactez-Nous</h3>
+				</div>
+			</a>
+		</section>
+	</div>
+</main>
+<?php
+	include 'footer.php'
+?>
---
+++ Accueil/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+		<title>Accueil</title>
+		<link rel="stylesheet" type="text/css" href="accueil.css">
+	</head>
+	<!--Tuto :https://www.youtube.com/watch?v=kbLfWKGVsMQ&ab_channel=DaniKrossing-->
+	<body>
+		<!-- Logo, navigation bar -->
+		<header>
+			<a href="accueil.php" ><img src="multimedia/Logo.jpg" alt="Logo : Groupe 72" class="header-brand"/></a>
+			<nav>
+				<ul>
+					<li><a href="./login.php"><!--<img src="./multimedia/user.png" class="user">-->Utilisateur</a>
+						<ul>
+							<li>
+								<a href="">Connexion</a>
+							</li>
+							<li>
+								<a href="mon_compte.php">Mon compte</a>
+							</li>
+							<li>
+								<a href="">Contactez-nous</a>
+							</li>
+						</ul>
+					</li>
+					<li><a href="./panier.php"><!--<img src="./multimedia/cart-icon.png" class="cart">-->Panier</a></li>
+					<li><a href="./produit.php">Produits</a></li>
+				</ul>
+
+				<!-- Mettre une barre de recherche ? -->
+				<form action="search.php" method="POST">
+					<input type="text" name="search" placeholder="search">
+					<button type="submit" name="submit-search">Search</button>
+				</form>
+
+			</nav>
+		</header>
+		<main>
+			<!--banner-->
+			<section class="index-banner">
+				<div class="vertical-center">
+					<h2> CLIQUEZ ET COLLECTEZ </h2>
+				</div>
+			</section>
+		</main>
+	</body>
+</html>
